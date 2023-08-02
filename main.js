16af67362257d5b4638d6ad2d5d0f38fa6da64da
boge <1037195898@qq.com>
2023-08-02 18:50:35 +0800

    delete trash

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 import "./lib/transform.js";
 import "./lib/mozilla-ast.js";
 import { minify } from "./lib/minify.js";
+import * as parse from "./lib/parse.js";
+import * as AST from "./lib/ast.js";
+import * as utils from "./lib/utils/index.js";
 
 export { minify } from "./lib/minify.js";
+export {AST, parse, utils};
 export { run_cli as _run_cli } from "./lib/cli.js";
+
+
 
 export async function _default_options() {
     const defs = {};
